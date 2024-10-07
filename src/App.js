884ab39a7b3d4f97e3c60f3a24c7bd6780efb155
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-10-07 15:41:06 +0200

    Redirects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
-// import NotFound from "./components/notFound";
+import NotFound from "./components/notFound";
 import "./App.css";
 
 class App extends Component {
@@ -16,11 +16,26 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
-            <Route path="/products/:id" render={(props) => <ProductDetails {...props} />}/>
-            <Route path="/products" render={(props) => <Products {...props} />} />
-            <Route path="/posts/:year?/:month?" render={(props) => <Posts {...props} />} />
+            <Route
+              path="/products/:id"
+              render={(props) => <ProductDetails {...props} />}
+            />
+            <Route
+              path="/products"
+              render={(props) => <Products {...props} />}
+            />
+            <Route
+              path="/posts/:year?/:month?"
+              render={(props) => <Posts {...props} />}
+            />
+            <Redirect from = "/messages"to="/posts" />
             <Route path="/admin" render={(props) => <Dashboard {...props} />} />
-            <Route path="/" render={(props) => <Home {...props} />} />
+            <Route
+              path="/not-found"
+              render={(props) => <NotFound {...props} />}
+            />
+            <Route path="/" exact render={(props) => <Home {...props} />} />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </div>
