0e688c83ee3a4462b587b0fe5bbd3aac83cad722
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-10-07 16:09:11 +0200

    Pragmatic navigation

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 
 class ProductDetails extends Component {
   handleSave = () => {
-    // Navigate to /products
+
+    this.props.history.push('/products');
+    
   };
 
   render() {
